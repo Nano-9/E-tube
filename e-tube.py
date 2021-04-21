0a0e462a511bec63c914725e3f5acbdb103a9563
Lucas Silva <69327287+lucas-Dk@users.noreply.github.com>
2021-04-21 15:22:27 -0300

    Update e-tube.py

--- e-tube.py
+++ e-tube.py
@@ -8,34 +8,40 @@ import urllib.request
 import os
 import banner
 
+print()
+os.system("clear")
 mensagem_boas_vindas = "Seja bem vindo ao E-tube!"
 for letra in mensagem_boas_vindas:
-	print(letra, end='', flush=True)
+	print("\033[1;36m{}\033[m".format(letra), end='', flush=True)
 	suspender(0.1)
+suspender(1.1)
 os.system("clear")
 
 try:
-	print("\033[1;31mVerificando se você está conectado na internet...\033[m")
-	suspender(1.2)
+	print("\033[1;31m[+] Verificando se você está conectado na internet...\033[m")
+	suspender(1.4)
 	connection = urllib.request.urlopen("https://www.google.com.br")
 except:
-	print("\n\033[1;31mERROR: Você não está conectado a internet :( \033[m\n")
+	print("\n\033[1;31m[ERROR]: Você não está conectado a internet :( \033[m\n")
 else:
 	os.system("clear")
 	while True:
 		banner.change_banner()
 		print("""
-Menu:
 
-\033[1;31m[\033[1;32m1\033[m\033[1;31m]\033[m - Baixar um vídeo MP4
-\033[1;31m[\033[1;32m2\033[m\033[1;31m]\033[m - Baixar uma playlist MP4
-\033[1;31m[\033[1;32m3\033[m\033[1;31m]\033[m - Baixar uma música MP3
-\033[1;31m[\033[1;32m4\033[m\033[1;31m]\033[m - Mudar cor do banner
-\033[1;31m[\033[1;32m5\033[m\033[1;31m]\033[m - Fale comigo
-\033[1;31m[\033[1;32mx\033[m\033[1;31m]\033[m - Sair
+> Meu GitHub: https://github.com/lucas-Dk
+
+MENU:
+
+\033[1;31m[\033[1;32m 1 \033[m\033[1;31m]\033[m - Baixar um vídeo MP4
+\033[1;31m[\033[1;32m 2 \033[m\033[1;31m]\033[m - Baixar uma playlist MP4
+\033[1;31m[\033[1;32m 3 \033[m\033[1;31m]\033[m - Baixar uma música MP3
+\033[1;31m[\033[1;32m 4 \033[m\033[1;31m]\033[m - Mudar cor do banner
+\033[1;31m[\033[1;32m 5 \033[m\033[1;31m]\033[m - Fale comigo
+\033[1;31m[\033[1;32m x \033[m\033[1;31m]\033[m - Sair
 			""")
 		try:
-			user = input("Faça sua escolha: ")
+			user = input("\n\033[1;33mO que você deseja:\033[m ")
 		except KeyboardInterrupt:
 			print("\nSaindo...")
 			suspender(1)
@@ -44,43 +50,37 @@ Menu:
 			user = int(user)
 			
 			if user == 1:
-
-				print("\nOpção de baixar apenas um vídeo está executando...\n")
+				print("\n\033[1;32m[+] Opção 1 > Download de um video.mp4\n\033[m")
+				print("\033[1;32m[+] O vídeo ficará salvo na pasta do script!\n\033[m")
 				try:
-					link = str(input("Url do vídeo: "))
+					link = str(input("\033[1;33mUrl do vídeo:\033[m "))
 				except KeyboardInterrupt:
 					print("\nSaindo...")
 					suspender(1)
 					sys.exit()# Essa linha não seria necessária, mas se ocorrer um bug inesperado, ela sai do programa
-				print("Buscando streams...")
 				url_video = link
 				yt = YouTube(url_video)
 
-				print("\033[1;31mMostrando todas as streams disponivel para download...\n")
-				print("Buscando a melhor e baixando a mesma...\033[m\n")
+				print("\n\033[1;31m[+] Buscando streams diponíveis para download...\033[m\n")
 
 				for streams in yt.streams:
-					print(streams)
-					suspender(0.6)
+					print("\033[1;31m[Vídeo]\033[m \033[1;33m{}\033[m".format(streams))
+					suspender(0.5)
 
-				print("\033[1;32mFazendo download da stream com a melhor qualidade disponível...\033[m")
+				print("\n\033[1;32m[+] Baixando o vídeo com a maior qualidade disponível...\033[m")
 				video = yt.streams.get_highest_resolution()
 				video.download()
-				print("\nDownload completo do vídeo! Verifique na pasta onde se encontra o script!")
-				print("Voltando ao menu...\n")
-				suspender(1.3)
+				print("\n\033[1;96m[+] Download completo do vídeo!\033[m")
+				print("\n\033[1;35m[+] Voltando ao menu...\n\033[m")
+				suspender(2)
+				os.system("clear")
 
 			elif user == 2:
-				print("\nOpção de baixar uma playlist está executando...\n")
-
-				print("\033[1;31mOBS: Confira se nenhum link está quebrado para que o download ocorra corretamente!\033[m\n")
-				print("\033[1;4mO que acontece se tiver um link quebrado?\033[m\n")
-				print("Ele vai baixando video por vídeo, quando chegar no video")
-				print("com a url quebrada, ele para de baixar os vídeos!\n")
-				input("Se entendeu dê enter para continuar")
-				print() # Espaço
+				print("\n\033[1;32m[+] Opção 2 > Download de uma Playlist.mp4\033[m\n")
+				print("\033[1;32m[+] Os vídeos ficarão salvos na pasta do script!\n\033[m")
+				print("\n\033[1;31m[OBS]: Link quebrado = para o download!\033[m\n")
 				try:
-					link_play = str(input("Url da playlist: "))
+					link_play = str(input("\033[1;33mUrl do vídeo:\033[m "))
 				except KeyboardInterrupt:
 					print("\nSaindo...")
 					suspender(1)
@@ -88,26 +88,29 @@ Menu:
 				else:
 					PLAYLIST_URL = link_play
 					playlist = Playlist(PLAYLIST_URL)
-					print("\n\033[1;32mBaixando sua playlist!")
-					print("Isso pode demorar um pouco...\033[m\n")
+					print("\n\033[1;32m[+] Baixando sua playlist...")
+					print("[+] Isso pode demorar um pouco...\033[m\n")
 					for url in playlist:
 						video = YouTube(url)
 						stream = video.streams.get_highest_resolution()
 						try:
 							stream.download(output_path='playlist')
 						except:
-							print("ERROR: Download quebrado por que encontrei uma url quebrada!")
+							print("[ERROR]: Download parado por que encontrei uma url quebrada!")
 						else:
 							pass
-					print("\nDownload Completo da playlist! Verifique na pasta onde se encontra o script!\n")
-					suspender(1.6)
+					print("\n\033[1;32m[+] Download Completo da playlist! Verifique na pasta onde se encontra o script!\n\033[m")
+					print("\n\033[1;35m[+] Voltando ao menu...\n\033[m")
+					suspender(2)
+					os.system("clear")
 
 			elif user == 3:
-				print("\nVocê escolheu a opção de baixar uma música!")
-				print("\n\033[1;4mOBS\033[m: Essa opção não converte o vídeo para .MP3")
-				print("apenas baixa somente o audio!\n")
+				print("\n\033[1;32m[+] Opção 3 > Download de uma Musica.mp3\033[m\n")
+				print("\033[1;32m[+] A música ficará salva na pasta do script!\n\033[m")
+				print("\n\033[1;31m[OBS]: Essa opção não converte o vídeo para .MP3")
+				print("Apenas baixa somente o audio!\033[m\n")
 				try:
-					link = str(input("Url do vídeo: "))
+					link = str(input("\033[1;33mUrl do vídeo:\033[m "))
 				except KeyboardInterrupt:
 					print("\nSaindo...")
 					suspender(1)
@@ -115,20 +118,20 @@ Menu:
 				else:
 					url_download = link
 					yt = YouTube(url_download)
-					print("\n\033[1;31mBuscando streams correspondentes...\033[m")
+					print("\n\033[1;31m[+] Buscando streams diponíveis para download...\033[m\n")
 					for streams in yt.streams:
-						print(streams)
-						suspender(1)
-					print("\n\033[1;92mFazendo o download....")
+						print("\033[1;31m[Música]\033[m \033[1;33m{}\033[m".format(streams))
+						suspender(0.5)
+					print("\n\033[1;92m[+] Fazendo o download...\033[m")
 					musica = yt.streams.get_highest_resolution()
 					try:
 						musica.download()
 					except:
-						print("Ocorreu um erro, verifique o link do video...")
+						print("\n\033[1;31m[OPS]: Ocorreu um erro, verifique o link do vídeo...\033[m")
 					else:
-						print("\nDownload completo\033[m\n")
-						print("Voltando ao menu...")
-						suspender(1)
+						print("\n\033[1;31m[+] Download completo!\033[m\n")
+						print("\n\033[1;35m[+] Voltando ao menu...\n\033[m")
+						suspender(2)
 						os.system("clear")
 
 			elif user == 4:
@@ -137,13 +140,14 @@ Menu:
 			elif user == 5:
 				os.system("clear")
 				print("""
-Para contato:
+\033[1;33m[+] Para contato:\033[m
+
 ---------------------------------------------------------------
 \033[1;4mE-mail:\033[m lucas2000bss@gmail.com
 \033[1;4mFacebook:\033[m https://www.facebook.com/Walker.Lxrd/
 ---------------------------------------------------------------
 
-fale comigo por uma dessas redes e terá contato comigo
+[+] Fale comigo por uma dessas redes e terá contato comigo
 				""")
 				try:
 					input("Dê enter para voltar ao menu... ")
@@ -153,7 +157,7 @@ fale comigo por uma dessas redes e terá contato comigo
 					sys.exit()# Essa linha não seria necessária, mas se ocorrer um bug inesperado, ela sai do programa
 				os.system("clear")
 			else:
-				print("Opção inválida! Tente novamente.")
+				print("\033[1;31mOpção inválida! Tente novamente.\033[m")
 				suspender(0.8)
 				os.system("clear")
 
@@ -163,7 +167,7 @@ fale comigo por uma dessas redes e terá contato comigo
 			sys.exit()
 
 		else:
-			print("Opção inválida! Tente novamente.")
+			print("\033[1;31mOpção inválida! Tente novamente.\033[m")
 			suspender(0.8)
 			os.system("clear")
 
